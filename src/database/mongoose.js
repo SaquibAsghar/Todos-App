04248bdc09aa9d1c206417db71481d455649e05e
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-06-02 01:08:47 +0530

    Refactor code

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
+const Todos = require("./models/todo-model");
+const Users = require("./models/user-model");
 
 // This will connect with the db: todos-app
 mongoose.connect("mongodb://localhost:27017/todos-app", {
@@ -8,61 +9,21 @@ mongoose.connect("mongodb://localhost:27017/todos-app", {
 	useFindAndModify: false,
 });
 
-const Todos = mongoose.model("Todos", {
-	title: {
-		type: String,
-		required: true,
-	},
-	isCompleted: {
-		type: Boolean,
-	},
-});
-
 const myTodo = new Todos({
 	title: "Buy some eggs",
 	isCompleted: false,
 });
 
-myTodo.save().then(() => console.log(myTodo));
-
-const User = mongoose.model("User", {
-	name: {
-		type: String,
-		required: true,
-		lowercase: true,
-	},
-	email: {
-		type: String,
-		required: true,
-		validate(val) {
-			if (!validator.isEmail(val)) {
-				throw new Error("Email is not correct");
-			}
-		},
-	},
-	password: {
-		type: String,
-		required: true,
-		minLength: 6,
-	},
-	age: {
-		type: Number,
-		validate(val) {
-			if (val < 0 || val === 0) {
-				throw new Error("Age must be a positive number.");
-			}
-		},
-	},
+const myUser = new Users({
+	name: "Priyank Sharma",
+	email: "psharma@asd.com",
+	password: "Pass1234word",
+	age: 44,
 });
 
-const myUser = new User({
-	name: "Syed Saquib Asghar",
-	email: "test@@asd.com",
-	password: "1234456",
-	age: 0,
-});
+// myTodo.save().then(() => console.log(myTodo));
 
 myUser
 	.save()
-	.then(()=>console.log("Printing my User:\n\t", myUser))
-	.catch((err) => console.log("Printing Error: ", {err}));
+	.then(() => console.log("Printing my User:\n\t", myUser))
+	.catch((err) => console.log("Printing Error: ", { err }));
