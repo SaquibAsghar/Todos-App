729c30fc21bb3a40a1c66f395f22fde6b6d3d44f
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-06-02 14:23:08 +0530

    added route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
 
 app.get("/", function (req, res) {
 	res.send("Welcome to homepage")
 });
+
+app.post('/users', (req, res)=>{
+	const createdUser = req.body
+	res.status(201).send(createdUser)
+})
 
 app.listen(port, () => console.log("running at port number", port));
