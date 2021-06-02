2fd1fef97aac5ca0240923add457827b0c87b720
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-06-02 16:16:13 +0530

    Update mongoose.js

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-const Todos = require("./models/todo-model");
-const Users = require("./models/user-model");
 
 // This will connect with the db: todos-app
 mongoose.connect("mongodb://localhost:27017/todos-app", {
@@ -9,21 +7,3 @@ mongoose.connect("mongodb://localhost:27017/todos-app", {
 	useFindAndModify: false,
 });
 
-const myTodo = new Todos({
-	title: "Buy some eggs",
-	isCompleted: false,
-});
-
-const myUser = new Users({
-	name: "Priyank Sharma",
-	email: "psharma@asd.com",
-	password: "Pass1234word",
-	age: 44,
-});
-
-// myTodo.save().then(() => console.log(myTodo));
-
-myUser
-	.save()
-	.then(() => console.log("Printing my User:\n\t", myUser))
-	.catch((err) => console.log("Printing Error: ", { err }));
